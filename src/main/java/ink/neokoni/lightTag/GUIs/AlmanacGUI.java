7fd88146cb736b23272f231078a68d8afeb9cd3e
neokoni <dulin081682@126.com>
2025-10-28 02:58:23 +0800

    feat(temp): almanac gui
    a big shit mountain it is, temporary

--- src/main/java/ink/neokoni/lightTag/GUIs/AlmanacGUI.java
+++ src/main/java/ink/neokoni/lightTag/GUIs/AlmanacGUI.java
@@ -1,4 +1,93 @@
 package ink.neokoni.lightTag.GUIs;
 
+import ink.neokoni.lightTag.DataStorage.Caches;
+import ink.neokoni.lightTag.DataStorage.Tags;
+import ink.neokoni.lightTag.GUIs.Base.ChestMenu;
+import ink.neokoni.lightTag.Utils.TagUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionType;
+
+import javax.swing.*;
+import java.util.List;
+
 public class AlmanacGUI {
+    private Player player;
+    private ChestMenu menu;
+    public AlmanacGUI(Player player) {
+        this.player = player;
+        menu = new ChestMenu(6);
+
+        ItemStack panel = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
+        ItemMeta panelMeta = panel.getItemMeta();
+        panelMeta.displayName(Component.text(""));
+        panel.setItemMeta(panelMeta);
+
+        ItemStack back = new ItemStack(Material.ARROW);
+        ItemMeta backMeta = back.getItemMeta();
+        backMeta.displayName(Component.text("返回上一页"));
+        back.setItemMeta(backMeta);
+
+        ItemStack previous = new ItemStack(Material.TIPPED_ARROW);
+        PotionMeta previousMeta = (PotionMeta) previous.getItemMeta();
+        previousMeta.displayName(Component.text("上一页"));
+        previousMeta.setBasePotionType(PotionType.SLOW_FALLING);
+        previous.setItemMeta(previousMeta);
+        previous.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+
+        ItemStack next = new ItemStack(Material.TIPPED_ARROW);
+        PotionMeta nextMeta = (PotionMeta) next.getItemMeta();
+        nextMeta.displayName(Component.text("下一页"));
+        nextMeta.setBasePotionType(PotionType.WIND_CHARGED);
+        next.setItemMeta(nextMeta);
+        next.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+
+        for (int i =0;i <9; i++) {
+            menu.put(panel);
+        }
+        List<Integer> panels = List.of(9, 17, 18, 26, 27, 35, 36, 44, 46, 47, 49, 51, 52,53, 54);
+        for (int i =0;i <menu.getSize(); i++) {
+            if (panels.contains(i)) {
+                menu.put(panel, i);
+            }
+        }
+
+        menu.put(back, 45);
+        menu.put(previous, 48);
+        menu.put(next, 50);
+
+        YamlConfiguration tags = Tags.getTags();
+        for (String s : tags.getKeys(false)) {
+            int id = Integer.valueOf(s);
+            Component tagView = TagUtils.getViewById(id);
+            String type = Tags.getTags().getString(id+".type");
+            boolean isAnimation = (type != null && type.equals("ANIMATION"));
+
+            ItemStack tagItem = new ItemStack(Material.NAME_TAG);
+            ItemMeta meta = tagItem.getItemMeta();
+            meta.displayName(tagView);
+            if (isAnimation)meta.setEnchantmentGlintOverride(true);
+            meta.lore(List.of(
+                    MiniMessage.miniMessage().deserialize("称号: ").append(tagView),
+                    MiniMessage.miniMessage().deserialize(isAnimation?"<yellow>动态称号":"<red>静态称号"),
+                    MiniMessage.miniMessage().deserialize("ID: "+id)));
+            tagItem.setItemMeta(meta);
+            menu.put(tagItem);
+        }
+
+        menu.setTitle("<yellow> 称号图鉴");
+
+    }
+
+    public void open() {
+        menu.open(player);
+        Caches.almanacGUI.put(menu.getInv(), this);
+    }
 }
